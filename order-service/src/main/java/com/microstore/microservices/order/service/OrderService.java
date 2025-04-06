41112dcbcc141d2977b7d4dc62a30925d6013b5f
Zavista <84294353+Zavista@users.noreply.github.com>
2025-04-06 19:22:01 -0400

    order microservice and mysql setup

---
+++ order-service/src/main/java/com/microstore/microservices/order/service/OrderService.java
@@ -0,0 +1,31 @@
+package com.microstore.microservices.order.service;
+
+import com.microstore.microservices.order.dto.OrderRequest;
+import com.microstore.microservices.order.model.Order;
+import com.microstore.microservices.order.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    @Autowired
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public void placeOrder(OrderRequest orderRequest) {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+        order.setPrice(orderRequest.price());
+        order.setSkuCode(orderRequest.skuCode());
+        order.setQuantity(orderRequest.quantity());
+        orderRepository.save(order);
+
+
+    }
+}
